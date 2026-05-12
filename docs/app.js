a976e2d55253e00bec61c67ea1e2203ca4baf274
Tommy_Bstz.V2 <tommaso.bastanzetti@liceischio.edu.it>
2026-05-12 19:54:55 +0200

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api/transactions';
+const API = 'https://financetrackerweb.onrender.com';
 
 async function fetchTransactions() {
     const response = await fetch(API);
